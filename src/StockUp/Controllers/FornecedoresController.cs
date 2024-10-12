42478c2558a521d8648b0f6408b74e1d5c679005
adriansodr3 <adriansodree@gmail.com>
2024-10-12 19:17:48 -0300

    adicionando pagina de fornecedores

---
+++ src/StockUp/Controllers/FornecedoresController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StockUp.Controllers
+{
+    public class FornecedoresController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
