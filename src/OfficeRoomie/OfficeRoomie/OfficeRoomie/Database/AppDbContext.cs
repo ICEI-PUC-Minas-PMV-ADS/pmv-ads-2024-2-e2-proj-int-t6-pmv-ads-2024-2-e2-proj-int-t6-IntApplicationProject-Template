4b0a5029ecc4e7a64ebaa10f7cb3e661a02e77c9
kevinsant13 <1522010@sga.pucminas.br>
2024-10-25 23:26:13 -0300

    Inserção RF - 05 (2)

--- src/OfficeRoomie/OfficeRoomie/OfficeRoomie/Database/AppDbContext.cs
+++ src/OfficeRoomie/OfficeRoomie/OfficeRoomie/Database/AppDbContext.cs
@@ -48,4 +48,6 @@ public class AppDbContext : DbContext
     public DbSet<Cliente> Clientes { get; set; }
 
     public DbSet<Administrador> Administradores { get; set; }
+
+    public DbSet<Cartao> Cartoes { get; set; }
 }
---
+++ src/OfficeRoomie/OfficeRoomie/OfficeRoomie/Migrations/20241026002037_AddCartaoTable.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OfficeRoomie.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCartaoTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "cartoes",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    numeroDoCartao = table.Column<string>(type: "TEXT", nullable: false),
+                    nomeDoTitular = table.Column<string>(type: "TEXT", nullable: false),
+                    validade = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    cvv = table.Column<int>(type: "INTEGER", nullable: false),
+                    idUsuario = table.Column<int>(type: "INTEGER", nullable: false),
+                    created_at = table.Column<string>(type: "TEXT", nullable: true),
+                    updated_at = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_cartoes", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "cartoes");
+        }
+    }
+}
