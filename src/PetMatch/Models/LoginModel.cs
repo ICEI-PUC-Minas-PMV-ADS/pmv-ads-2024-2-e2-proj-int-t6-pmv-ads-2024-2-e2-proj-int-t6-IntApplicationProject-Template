acbb77dd58ac6e971c4fa927b7e1d432338a49f7
Rodrigo Abreu Amorim <Ramorim13@yahoo.com.br>
2024-11-19 19:54:49 -0300

    Update projeto base

---
+++ src/PetMatch/Models/LoginModel.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PetMatch.Models
+{
+    public class LoginViewModel
+    {   
+        [Required]
+        public int? Nome { get; set; }
+
+        [Required(ErrorMessage =" O Campo Email é obrigatório")]
+        [EmailAddress(ErrorMessage ="Insira um endereço de email válido")]
+        public string? Email { get; set; }
+
+        [Required (ErrorMessage =" O campo senha é obrigatorio")]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+    }
+
+    public class ForgotPasswordViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string? Email { get; set; }
+    }
+
+    public class ResetPasswordViewModel
+    {
+        [Required]
+        public string? Token { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string? Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "As senhas não coincidem.")]
+        public string? ConfirmPassword { get; set; }
+    }
+}
--- src/pet-match/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace pet_match.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
